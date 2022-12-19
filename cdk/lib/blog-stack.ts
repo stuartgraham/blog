9b4a0ea416ac3a0108f467f5b301d953f4e19a36
Stuart Graham <stuart@stuart-graham.com>
2022-12-19 21:22:25 +0000

    Initial build

--- cdk/lib/blog-stack.ts
+++ cdk/lib/blog-stack.ts
@@ -54,7 +54,6 @@ export class BlogStack extends cdk.Stack {
       }),
     );
 
-    //
     // Cfn Output
     new cdk.CfnOutput(this, 'blogCloudfrontDistributionId', {
       value: blogCloudfrontDistro.distributionId,
