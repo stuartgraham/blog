3e66933d60454fb8d07b441c4485d75b372937c8
Stuart Graham <stuart@stuart-graham.com>
2022-12-19 17:18:57 +0000

    Update stack

--- cdk/lib/blog_cdk-stack.ts
+++ cdk/lib/blog_cdk-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class BlogCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,12 +15,23 @@ export class BlogCdkStack extends cdk.Stack {
     });
 
     // ACM
-    
+    const arn = process.env.CERTIFICATE_ARN;
+    const certificate = acm.Certificate.fromCertificateArn(this, 'Certificate', arn);
+
     // Cloudfront
-    
+    new cloudfront.Distribution(this, 'blogCFDistribution', {
+      defaultBehavior: { origin: new origins.S3Origin(blogBucket) },
+    });
+
     // IAM Role for Lambda Edge
 
     // Lambda
+
+    new cdk.CfnOutput(this, 'bucketName', {
+      value: blogBucket.bucketName,
+      description: 'blog bucket',
+      exportName: 'bucket_name',
+    });
 
   }
 }
