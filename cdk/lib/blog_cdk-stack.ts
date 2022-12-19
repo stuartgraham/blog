2d0bd2df6681994958d25fed21e1b26be2b6ce15
Stuart Graham <stuart@stuart-graham.com>
2022-12-19 16:23:22 +0000

    Test bucket

--- cdk/lib/blog_cdk-stack.ts
+++ cdk/lib/blog_cdk-stack.ts
@@ -1,12 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class BlogCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // S3 bucket
+    // S3 bucket for AWS Instance Scheduler
+    const blogBucket = new s3.Bucket(this, 'blogBucket', {
+      bucketName: process.env.BUCKET_NAME
+    });
 
     // ACM
     
@@ -15,6 +18,8 @@ export class BlogCdkStack extends cdk.Stack {
     // IAM Role for Lambda Edge
 
     // Lambda
+
+    // Test
 
 
   }
