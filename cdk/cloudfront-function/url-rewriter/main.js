80d263c8fc1facb76d0f1021509f0ef84735e2a3
Stuart Graham <stuart@stuart-graham.com>
2023-01-07 02:07:10 +0000

    Debug

--- cdk/cloudfront-function/url-rewriter/main.js
+++ cdk/cloudfront-function/url-rewriter/main.js
@@ -1,14 +1,8 @@
 function handler(event) {
-    var response = event.response;
     var request = event.request;
-
     console.log(request);
-    console.log(response);
-
-    // Extract the request from the CloudFront event
-    console.log(request.uri);
-
     var uri = request.uri;
+    console.log(request.uri);
 
     // Check whether the URI is missing a file name.
     if (uri.endsWith('/')) {
