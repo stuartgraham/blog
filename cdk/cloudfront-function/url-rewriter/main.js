0fe6d18b8c97685a6e064af66fcb083697b085df
Stuart Graham <stuart@stuart-graham.com>
2023-01-07 01:48:54 +0000

    Debug

--- cdk/cloudfront-function/url-rewriter/main.js
+++ cdk/cloudfront-function/url-rewriter/main.js
@@ -1,17 +1,11 @@
 function handler(event) {
-    var request = event.request;
     var response = event.response;
 
-    console.log(request);
+    //console.log(request);
     console.log(response);
 
     // Extract the request from the CloudFront event
-    var request = event.Records[0].cf.request;
-
-    // Extract the URI from the request
-    var sentUri = request.uri;
-    console.log(sentUri);
-
+    console.log(response.uri);
 
     return response;
 }
